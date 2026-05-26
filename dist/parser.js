205f9aaa56a0d3439046cf3ded78101e7786f5f1
rladeiraFatec <ricardo.ladeira@fatec.sp.gov.br>
2026-05-26 18:37:24 -0300

    Mesmo programa, usando switch/case,

--- dist/parser.js
+++ dist/parser.js
@@ -1,34 +1,20 @@
 import { Stack } from "./stack.js";
 function limparTag(tag) {
-    let novaTag = "";
-    let i = 0;
-    while (i < tag.length && tag[i] !== " ") {
-        novaTag += tag[i];
-        i++;
-    }
-    return novaTag.toLowerCase();
+    return tag.split(" ")[0].toLowerCase();
 }
 function escaparLatex(caractere) {
-    if (caractere === "&") {
-        return "\\&";
+    switch (caractere) {
+        case '&': return '\\&';
+        case '%': return '\\%';
+        case '$': return '\\$';
+        case '#': return '\\#';
+        case '_': return '\\_';
+        default: return caractere;
     }
-    if (caractere === "%") {
-        return "\\%";
-    }
-    if (caractere === "$") {
-        return "\\$";
-    }
-    if (caractere === "#") {
-        return "\\#";
-    }
-    if (caractere === "_") {
-        return "\\_";
-    }
-    return caractere;
 }
 export function converter(html) {
     let resultado = "";
-    let pilha = new Stack();
+    const pilha = new Stack();
     let i = 0;
     // Adiciona preâmbulo do documento
     resultado = "\\documentclass{article}\n";
@@ -38,106 +24,108 @@ export function converter(html) {
     while (i < html.length) {
         if (html[i] === "<") {
             let j = i + 1;
-            let tag = "";
+            let tagAcumulada = "";
             while (j < html.length && html[j] !== ">") {
-                tag += html[j];
+                tagAcumulada += html[j];
                 j++;
             }
-            tag = limparTag(tag);
+            const tag = limparTag(tagAcumulada);
             if (j >= html.length) {
                 resultado += html[i];
                 i = j;
                 continue;
             }
-            if (tag === "p") {
-                pilha.push("p");
-            }
-            else if (tag === "/p") {
-                if (pilha.vazia() || pilha.topo() !== "p") {
+            if (tag.startsWith("/")) {
+                let esperado = "";
+                let tokenFechamento = "";
+                switch (tag) {
+                    case '/p':
+                        tokenFechamento = "\n\n";
+                        esperado = "p";
+                        break;
+                    case '/b':
+                    case '/strong':
+                        tokenFechamento = "}";
+                        esperado = "b";
+                        break;
+                    case '/i':
+                    case '/em':
+                        tokenFechamento = "}";
+                        esperado = "i";
+                        break;
+                    case '/ul':
+                        tokenFechamento = "\\end{itemize}\n";
+                        esperado = "ul";
+                        break;
+                    case '/ol':
+                        tokenFechamento = "\\end{enumerate}\n";
+                        esperado = "ol";
+                        break;
+                    case '/li':
+                        tokenFechamento = "\n";
+                        esperado = "li";
+                        break;
+                    case '/h1':
+                        tokenFechamento = "}\n";
+                        esperado = "h1";
+                        break;
+                    case '/h2':
+                        tokenFechamento = "}\n";
+                        esperado = "h2";
+                        break;
+                    default:
+                        i = j;
+                        i++;
+                        continue;
+                }
+                if (pilha.vazia() || pilha.topo() !== esperado) {
                     return "Erro de Sintaxe: Tag fechada incorretamente.";
                 }
-                resultado += "\n\n";
+                resultado += tokenFechamento;
                 pilha.pop();
             }
-            else if (tag === "b" || tag === "strong") {
-                resultado += "\\textbf{";
-                pilha.push("b");
-            }
-            else if (tag === "/b" || tag === "/strong") {
-                if (pilha.vazia() || pilha.topo() !== "b") {
-                    return "Erro de Sintaxe: Tag fechada incorretamente.";
+            else {
+                switch (tag) {
+                    case 'p':
+                        pilha.push("p");
+                        break;
+                    case 'b':
+                    case 'strong':
+                        resultado += "\\textbf{";
+                        pilha.push("b");
+                        break;
+                    case 'i':
+                    case 'em':
+                        resultado += "\\textit{";
+                        pilha.push("i");
+                        break;
+                    case 'ul':
+                        resultado += "\\begin{itemize}\n";
+                        pilha.push("ul");
+                        break;
+                    case 'ol':
+                        resultado += "\\begin{enumerate}\n";
+                        pilha.push("ol");
+                        break;
+                    case 'li':
+                        resultado += "\\item ";
+                        pilha.push("li");
+                        break;
+                    case 'h1':
+                        resultado += "\\section{";
+                        pilha.push("h1");
+                        break;
+                    case 'h2':
+                        resultado += "\\subsection{";
+                        pilha.push("h2");
+                        break;
+                    case 'br':
+                    case 'br/':
+                        resultado += "\\\\\n";
+                        break;
+                    default:
+                        break;
                 }
-                resultado += "}";
-                pilha.pop();
-            }
-            else if (tag === "i" || tag === "em") {
-                resultado += "\\textit{";
-                pilha.push("i");
-            }
-            else if (tag === "/i" || tag === "/em") {
-                if (pilha.vazia() || pilha.topo() !== "i") {
-                    return "Erro de Sintaxe: Tag fechada incorretamente.";
-                }
-                resultado += "}";
-                pilha.pop();
-            }
-            else if (tag === "ul") {
-                resultado += "\\begin{itemize}\n";
-                pilha.push("ul");
-            }
-            else if (tag === "/ul") {
-                if (pilha.vazia() || pilha.topo() !== "ul") {
-                    return "Erro de Sintaxe: Tag fechada incorretamente.";
-                }
-                resultado += "\\end{itemize}\n";
-                pilha.pop();
-            }
-            else if (tag === "ol") {
-                resultado += "\\begin{enumerate}\n";
-                pilha.push("ol");
-            }
-            else if (tag === "/ol") {
-                if (pilha.vazia() || pilha.topo() !== "ol") {
-                    return "Erro de Sintaxe: Tag fechada incorretamente.";
-                }
-                resultado += "\\end{enumerate}\n";
-                pilha.pop();
-            }
-            else if (tag === "li") {
-                resultado += "\\item ";
-                pilha.push("li");
-            }
-            else if (tag === "/li") {
-                if (pilha.vazia() || pilha.topo() !== "li") {
-                    return "Erro de Sintaxe: Tag fechada incorretamente.";
-                }
-                resultado += "\n";
-                pilha.pop();
-            }
-            else if (tag === "br" || tag === "br/") {
-                resultado += "\\\\\n";
-            }
-            else if (tag === "h1") {
-                resultado += "\\section{";
-                pilha.push("h1");
-            }
-            else if (tag === "/h1") {
-                if (pilha.vazia() || pilha.topo() !== "h1") {
-                    return "Erro de Sintaxe: Tag fechada incorretamente.";
-                }
-                resultado += "}\n";
-                pilha.pop();
-            }
-            else if (tag === "h2") {
-                resultado += "\\subsection{";
-                pilha.push("h2");
-            }
-            else if (tag === "/h2") {
-                if (pilha.vazia() || pilha.topo() !== "h2") {
-                    return "Erro de Sintaxe: Tag fechada incorretamente.";
-                }
-                resultado += "}\n";
-                pilha.pop();
             }
             i = j;
         }
